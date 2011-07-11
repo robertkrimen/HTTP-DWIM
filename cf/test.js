d545ca20e4a7904fd36a8ccd72d6e65883d029c0
robertkrimen <robertkrimen@gmail.com>
2011-07-11 16:47:25 -0700

    Added promise exception test

--- cf/test.js
+++ cf/test.js
@@ -1,3 +1,35 @@
 test( 'cf', function(){
     ok( true );
 } );
+
+test( 'promise', function(){
+    var value;
+
+    value = 0;
+    $.ajax({
+        type: 'GET',
+        url: 'http://google.com',
+        async: false
+    })
+    .success( function(){
+        value += 1;
+    } )
+    .error( function(){
+        throw "Xyzzy0";
+        value += 1;
+    } )
+    .complete( function(){
+        throw "Xyzzy";
+    } )
+    .complete( function(){
+        value += 1;
+        throw "Xyzzy1";
+    } )
+    ;
+    equal( value, 2 );
+
+} );
+
+test( 'afterPromise', function(){
+    ok( true )
+} );
